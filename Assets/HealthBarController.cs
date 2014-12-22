b9cad3f2ef3b16ba32c2f32fa1b282cda393a6ec
Connor Cleary <connor.cleary@snapengage.com>
2014-12-22 09:23:31 -0700

    WIP Use pixels for health bar system maybe.

---
+++ Assets/HealthBarController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarController : MonoBehaviour {
+
+	public Sprite black;
+	public Sprite red;
+	public Sprite green;
+	
+	private SpriteRenderer renderBlack;
+	private SpriteRenderer renderRed;
+	private SpriteRenderer renderGreen;
+	
+	private GameObject renderObjectBlack;
+	private GameObject renderObjectRed;
+	private GameObject renderObjectGreen;
+
+	// Use this for initialization
+	void Start () {
+		
+		renderObjectBlack = CreateRendererObject ("RenderObjectBlack");
+		renderObjectRed = CreateRendererObject ("RenderObjectRed");
+		renderObjectGreen = CreateRendererObject ("RenderObjectGreen");
+		
+		renderBlack = AddRenderer (renderObjectBlack, black);
+		renderRed = AddRenderer (renderObjectBlack, red);
+		renderGreen = AddRenderer (renderObjectBlack, green);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private GameObject CreateRendererObject(string name)
+	{
+		GameObject obj = new GameObject ();
+		obj.name = name;
+		obj.transform.parent = transform;
+		return obj;
+	}
+
+	private SpriteRenderer AddRenderer(GameObject addToThis, Sprite sprite)
+	{
+		SpriteRenderer renderer = addToThis.AddComponent("SpriteRenderer") as SpriteRenderer;
+		renderer.sprite = sprite;
+		return renderer;
+	}
+}
