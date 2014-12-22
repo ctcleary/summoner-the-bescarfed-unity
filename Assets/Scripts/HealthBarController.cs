1a2853ee555731fdfcfec0388c61d9772634acd2
Connor Cleary <connor.cleary@snapengage.com>
2014-12-22 07:45:33 -0700

    WIP Health Bars.

--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class HealthBarController : MonoBehaviour {
 
-	private Texture2D barBackground;
-	private Texture2D barColor;
+	private Texture2D black;
+	private Texture2D green;
+	private Texture2D red;
 
 	// Use this for initialization
 	void Start () {
-		barBackground = new Texture2D();
+		black = new Texture2D (1, 1);
+		black.SetPixel (1, 1, Color.black);
+		black.Apply ();
 	}
+
+	void OnGUI()
+	{
+		
+		GUI.DrawTexture (new Rect (transform.position.x, transform.position.y, 300, 300), black);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
