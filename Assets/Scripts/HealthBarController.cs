7b2624a52a96adb6765198f7861cb904cfedfe43
Connor T Cleary <connorcleary@gmail.com>
2014-12-22 07:26:25 -0700

    WIP Health Bars.

---
+++ Assets/Scripts/HealthBarController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarController : MonoBehaviour {
+
+	private Texture2D barBackground;
+	private Texture2D barColor;
+
+	// Use this for initialization
+	void Start () {
+		barBackground = new Texture2D();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
