0f498d7883edde9faa3ce53bb87928ef84a9033e
Connor T Cleary <connorcleary@gmail.com>
2015-12-13 11:05:31 -0800

    Implement deadsimple pause behavior.

---
+++ Assets/Scripts/GlobalKeyListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalKeyListener : MonoBehaviour {
+
+    bool isPaused = false; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused && Time.timeScale != 0)
+        {
+            Time.timeScale = 0;
+        } else if (!isPaused && Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
